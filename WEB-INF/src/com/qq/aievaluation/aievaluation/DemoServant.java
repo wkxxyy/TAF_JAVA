0e8d41b8964b42cd727b5338d52fbd8d53f2a249
wkxxyy <5794431@qq.com>
2019-05-10 10:04:53 +0800

    first commit

---
+++ WEB-INF/src/com/qq/aievaluation/aievaluation/DemoServant.java
@@ -0,0 +1,14 @@
+package com.qq.aievaluation.aievaluation;
+
+import com.qq.cloud.taf.common.annotation.*;
+import com.qq.cloud.taf.common.support.Holder;
+
+/**
+ * Generated code, DO NOT modify it!
+ * @author jce2java
+ */
+@JceService
+public interface DemoServant {
+
+	public int test();
+}
---
+++ WEB-INF/src/com/qq/aievaluation/aievaluation/impl/DemoServantImpl.java
@@ -0,0 +1,8 @@
+package com.qq.aievaluation.aievaluation.impl;
+
+public class DemoServantImpl {
+
+    public int test(){
+        return 520;
+    }
+}
